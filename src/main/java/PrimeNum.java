beb80ba8401c7bc73ab5978e55606a40fda31c22
Snehaittoop <snehaittoop868@gmail.com>
2022-07-27 16:32:05 +0530

    added

--- src/main/java/PrimeNum.java
+++ src/main/java/PrimeNum.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
+
 public class PrimeNum {
+    public static void main(String[] args) {
+        Scanner input =new Scanner(System.in);
+        System.out.println("enter the number");
+        int n=input.nextInt();
+        boolean isPrime=true;
+        for(int i=2;i<n;i++){
+            if(n%i==0){
+                isPrime=false;
+                break;
+            }
+        }
+        System.out.println(isPrime);
+    }
 }
